06e570b84bb0be5241cf02a8cbfbe1728d59872d
Sebastian <sebastian.luque.pl@gmail.com>
2019-07-07 13:20:53 -0500

    changed database name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://mongo:27017/padron_sunat", { useNewUrlParser: true })
+  .connect("mongodb://mongo:27017/bd_sunat", { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
